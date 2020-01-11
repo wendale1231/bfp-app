27ebabc586b05b28c5d8a71afbbcf98b3960d048
wendale1231 <spooderm5@gmail.com>
2020-01-11 11:38:33 +0800

    Update To show th image note: this is alternative code and algorithm(temporary)

--- www/js/controller/getreports.js
+++ www/js/controller/getreports.js
@@ -1,15 +1,20 @@
 $("#getreports").click(function(){
 	console.log("Loading Feed");
-	$.ajax({
-		type: "POST",
-		dataType: "html",
-		url: "http://halfbyte.000webhostapp.com/mobile-server/get-reports.php",
-		data:{
-			'user_id': document.getElementById("user_id").val,
-		},
-		success: function(html){
-			console.log(html);
-			document.getElementById("feed").innerHTML = html;
-		}
-	});
+	// cordova.InAppBrowser.open(encodeURI('http://ja.m.wikipedia.org/wiki/ハングル'), '_blank', 'hideurlbar=');
+	// navigator.app.loadUrl('http://halfbyte.000webhostapp.com/mobile-server/get-reports.php?user_id=' + document.getElementById("user_id").val, { openExternal:true });
+	// window.open('http://www.kidzout.com', '_system'); 
+	// return false;
+	cordova.InAppBrowser.open('http://halfbyte.000webhostapp.com/mobile-server/get-reports.php?user_id=' + document.getElementById("user_id").val, '_blank ', 'location=no,closebuttoncaption=Close');
+	// $.ajax({
+	// 	type: "POST",
+	// 	dataType: "html",
+	// 	url: "http://halfbyte.000webhostapp.com/mobile-server/get-reports.php",
+	// 	data:{
+	// 		'user_id': document.getElementById("user_id").val,
+	// 	},
+	// 	success: function(html){
+	// 		console.log(html);
+	// 		document.getElementById("feed").innerHTML = html;
+	// 	}
+	// });
 });
